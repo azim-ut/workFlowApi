017a81819bc5fc546039e9a8c10754f9cec214dd
Aleksandr Azimut <9420183@gmail.com>
2025-09-08 21:59:05 +0300

    version update

---
+++ src/PluginInstall.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace src;
+
+use Relay\Event;
+
+class PluginDemo implements PluginInterface, EventSubscriberInterface
+{
+    protected Composer $composer;
+
+    protected IOInterface $io;
+
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [ PluginEvents::INIT => [ 'pluginDemoMethod', 0 ] ];
+    }
+
+    public function pluginDemoMethod(Event $event): void
+    {
+        $this->io->write(PHP_EOL.'<options=bold>========= Demo plugin =========</>');
+        $this->io->write('<info>Congrats, your plugin works! :)</info>');
+        $this->io->write('<options=bold>===============================</>'.PHP_EOL);
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
+}
